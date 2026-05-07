1f796c83ff0ef99bb057f704f34407029b80e0cc
mousty <moustapha.dev03@gmail.com>
2026-05-07 11:39:02 +0200

    initial commit

---
+++ src/main/java/com/hireboost/api/HireboostApiApplication.java
@@ -0,0 +1,13 @@
+package com.hireboost.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireboostApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HireboostApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hireboost/api/HireboostApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hireboost.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireboostApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
