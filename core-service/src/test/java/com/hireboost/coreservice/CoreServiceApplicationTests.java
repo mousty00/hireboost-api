8757187321d39dfa7cd0edb566507baed82e5ee6
mousty <moustapha.dev03@gmail.com>
2026-05-09 17:39:52 +0200

    [MGMT]  define core service

---
+++ core-service/src/test/java/com/hireboost/coreservice/CoreServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hireboost.coreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class CoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ core-service/src/test/java/com/hireboost/coreservice/TestCoreServiceApplication.java
@@ -0,0 +1,11 @@
+package com.hireboost.coreservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CoreServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
