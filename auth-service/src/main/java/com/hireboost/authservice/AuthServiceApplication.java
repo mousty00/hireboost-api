dd39c410af4410e249e1badf69fcb4f5d4d7bc78
Rohit <verma.rohit.1203@gmail.com>
2026-05-29 23:38:05 +0200

    [FEAT] docker setup #14

--- auth-service/src/main/java/com/hireboost/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/hireboost/authservice/AuthServiceApplication.java
@@ -2,12 +2,36 @@ package com.hireboost.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 
+    //    TODO remove it
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/*").permitAll() // Public endpoint
+                        .anyRequest().authenticated() // Everything else requires authentication
+                )
+                .formLogin(Customizer.withDefaults()) // Keep default login page
+                .build();
+    }
+
+    //    TODO remove it
+    @GetMapping
+    public String getHello() {
+        return "Hello from auth";
+    }
 }
